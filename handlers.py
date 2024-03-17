131f5024db8bc100d7d6fea0285a8f1e023811a3
Pavel Sergeev <enducjdksn@gmail.com>
2024-03-17 13:12:40 +0300

    05

--- handlers.py
+++ handlers.py
@@ -15,7 +15,6 @@ router = Router()
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
-    await msg.answer(text.menu, reply_markup=kb.menu)
 
 @router.message(F.text == "Меню")
 @router.message(F.text == "Выйти в меню")
