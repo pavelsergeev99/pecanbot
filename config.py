590df80322e4616d40ed866b0e8b0a6270a15599
Pavel Sergeev <enducjdksn@gmail.com>
2024-03-18 19:33:24 +0300

    02

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
 
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='.venv/.env', env_file_encoding='utf-8')
 
 config_settings = Settings()
