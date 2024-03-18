98276723caa8837887f8adcf4d6ec002b6831c87
Pavel Sergeev <enducjdksn@gmail.com>
2024-03-18 19:23:29 +0300

    01

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
 class Settings(BaseSettings):
