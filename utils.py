bb218d6bd9006a88ad37c70806a6a4f082a3921f
Pavel Sergeev <enducjdksn@gmail.com>
2024-03-19 16:19:16 +0300

    07

--- utils.py
+++ utils.py
@@ -1,11 +1,8 @@
-import logging
-
 import text
 
 async def generate_pecanbot_count(prompt) -> int:
-    try:
-        if isinstance(int(prompt), int):
-            return int(prompt) // text.text_pecanbon_price
-    except Exception as e:
-            logging.error(e)
-            return 0
+    prompt_type = type(prompt)
+    if (prompt_type == type(1)):
+         return int(prompt) // text.text_pecanbon_price
+    else:
+         return 0
