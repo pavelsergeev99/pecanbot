834ec0834732939da4c5f58f12abcd911683eab1
Pavel Sergeev <enducjdksn@gmail.com>
2024-03-19 16:25:29 +0300

    08

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import text
 
 async def generate_pecanbot_count(prompt) -> int:
     prompt_type = type(prompt)
-    if (prompt_type == type(1)):
+    if (prompt_type == "<class 'int'>" or prompt_type == "<class 'float'>"):
          return int(prompt) // text.text_pecanbon_price
     else:
          return 0
